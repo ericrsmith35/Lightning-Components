8a3584825b18e389d1395743d8cf4c92ac003bf8
Eric Smith <ericrsmith35@gmail.com>
2019-01-26 10:51:33 -0500

    Add Comments

--- src/aura/ersLongTextArea/ersLongTextAreaController.js
+++ src/aura/ersLongTextArea/ersLongTextAreaController.js
@@ -1,11 +1,15 @@
 ({   
     doInit : function(component, event, helper) {
+        
+        // Set component width
         var init = (component.get("v.default") == "On");
         component.set("v.checked", init);
         var size = "slds-size_4-of-4 slds-m-top_xxx-small";
         if (component.get("v.width") == "Half"){
             size = "slds-size_2-of-4 slds-m-top_xxx-small";
         }
+        
+        // Set component height
         component.set("v.sldsSize", size);
         var height = "largeArea8";
         if (component.get("v.height") == "Small"){
@@ -15,16 +19,20 @@
             height = "largeArea12";
         }
         component.set("v.areaHeight", height);
+        
+        // Set View/Edit mode
         component.set("v.editMode", (component.get("v.mode") == "Edit"));
     },
 
     handleLoad : function(component, event, helper) {
+        // Save current value of the field
         var currentValue = component.find("LTAField").get("v.value");
         component.set("v.fieldValue", currentValue);
         component.set("v.loaded", true);
     },
     
     handleSubmit : function(component, event, helper) {
+        // Update record with new field value
         var newValue = component.get("v.fieldValue");
         component.find("LTAField").set("v.value", newValue);
         event.preventDefault(); 							// Prevent default submit
@@ -35,6 +43,7 @@
     },
     
     handleSaved: function(component, event, helper) {
+        // Show Saved Success Message
         component.find("notificationsLibrary").showToast({
             "variant": "success",
             "title": "Success!",
@@ -43,10 +52,12 @@
     },
     
     handleError: function(component, event, helper) {
+        // Handle errors
         console.log("Error"); 
     },
     
     handleToggleChanged: function(component, event, helper) {
+        // Process Toggle Button
         console.log("Checked: " + component.get("v.checked"));
     },
         
