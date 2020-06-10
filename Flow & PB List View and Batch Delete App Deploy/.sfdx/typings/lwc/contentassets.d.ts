29f7cdfdd8c2ed9ce32aa55807923cf2b3f04c09
ericrsmith35 <ericrsmith35@gmail.com>
2020-06-10 14:29:20 -0400

    deploy folder

---
+++ Flow & PB List View and Batch Delete App Deploy/.sfdx/typings/lwc/contentassets.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/contentAssetUrl/X430347" {
+    var X430347: string;
+    export default X430347;
+}
---
+++ Flow & PB List View and Batch Delete App Deploy/.sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,8 @@
+declare module "@salesforce/resourceUrl/BulkDeleteImage" {
+    var BulkDeleteImage: string;
+    export default BulkDeleteImage;
+}
+declare module "@salesforce/resourceUrl/DeleteTrash" {
+    var DeleteTrash: string;
+    export default DeleteTrash;
+}
